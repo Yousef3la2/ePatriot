3a0221d18dbae3712d6529671ba4033c5d085039
Yousef Alaa <33986623+Yousef3la2@users.noreply.github.com>
2025-11-01 19:13:49 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import LandingPage from './Components/LandingPage'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <LandingPage/>
+       </div>
+    </>
+  )
+}
+
+export default App
