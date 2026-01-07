3ec44400e4bdf667835cffcc715eb2cf107e04aa
naghamhaimanahmed <naghamhaiman2@gmail.com>
2026-01-07 22:29:39 +0200

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./Components/pages/HomePage";
 import LoginPage from "./Components/pages/LoginPage";
 import ForgotPasswordPage from "./Components/pages/ForgotPasswordPage";
 import SignUpPage from "./Components/pages/SignUpPage";
+import TeacherManagement from "./Components/pages/TeacherManagement";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/teachers" element={<TeacherManagement />} />
     </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-import SchoolLogin from './Components/pages/SchoolLogin.jsx' ;
+import SchoolLogin from './Components/pages/SchoolLogin.jsx';
 import SchoolPage from './Components/pages/SchoolPage.jsx';
 import TeacherManagementPage from './Components/pages/TeacherManagement.jsx';
 import ContentLibraryPage from './Components/pages/ContentLibrary.jsx';
