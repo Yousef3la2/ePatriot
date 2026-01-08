d984d93dfd871e19087b8f130d42a9807922b669
naghamhaimanahmed <naghamhaiman2@gmail.com>
2026-01-08 18:13:55 +0200

    Teacher Details

--- src/Components/pages/TeacherManagement.jsx
+++ src/Components/pages/TeacherManagement.jsx
@@ -84,7 +84,9 @@ const TeacherManagement = () => {
               </div>
 
               
-              <button className="show-details-btn">Show Details</button>
+              <Link to={`/teachers/${teacher.id}`} style={{ textDecoration: 'none' }}>
+                <button className="show-details-btn">Show Details</button>
+              </Link>
             </div>
           ))}
         </div>
